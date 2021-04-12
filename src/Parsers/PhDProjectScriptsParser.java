445fb54711954f6835cf29c94b5acadfd83891df
Davide <altieri.davide.98@gmail.com>
2021-04-12 15:10:59 +0200

    Parser

--- src/Parsers/PhDProjectScriptsParser.java
+++ src/Parsers/PhDProjectScriptsParser.java
@@ -2,13 +2,24 @@ package Parsers;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.sql.Array;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import models.Method;
+import models.Class;
+import models.Package;
+import models.Variable;
+import database.DbHandler;
+import models.Smell;
 
 public class PhDProjectScriptsParser {
 
     String smell, messageChainsTemp, idCommit, projectUrl;
-    ArrayList<String> classes = new ArrayList<>();
-    String analyzableFileName = "result.txt";
+    ArrayList<String> classes, pathClasses, packages;
+    Class c1, c2;
+    Method m;
+    Package p;
+    Variable v;
 
     public PhDProjectScriptsParser(String idCommit, String projectUrl) {
         this.idCommit = idCommit;
@@ -22,7 +33,6 @@ public class PhDProjectScriptsParser {
             String line;
 
             boolean continueReading = true;
-            boolean startAnalysis = false;
 
             do {
 
@@ -30,8 +40,6 @@ public class PhDProjectScriptsParser {
                 System.out.println(line);
 
                 switch (line) {
-
-                    case "***START BAD SMELLS TRANSCRIPTION***" -> startAnalysis = true;
 
                     case "Switch Statement Bad Smell was found in:" -> smell = "Switch Statement";
                     case "Speculative Generality Bad Smell was found in:" -> smell = "Speculative Generality";
@@ -42,7 +50,7 @@ public class PhDProjectScriptsParser {
                     case "***END BAD SMELLS TRANSCRIPTION***" -> continueReading = false;
 
                     default -> {
-                        if (!line.isBlank() && startAnalysis) {
+                        if (!line.isBlank()) {
                             analyzeAndWrite(line);
                         }
                     }
@@ -64,10 +72,13 @@ public class PhDProjectScriptsParser {
 
             //Delete previous paths and split paths in case of more directoy per string.
             classes = new ArrayList<>();
+            pathClasses = new ArrayList<>();
+            packages = new ArrayList<>();
             String[] paths = line.split("&");
 
             //Obtain class path in project's source.
             for (String p : paths) {
+                pathClasses.add(p);
                 classPath = "";
                 beginWritePath = false;
 
@@ -77,6 +88,7 @@ public class PhDProjectScriptsParser {
                 if (p.contains("§src§")) {
 
                     for (String s : path) {
+                        packages.add(path[path.length-2]);
                         if (!beginWritePath & s.equals("src")) {
                             beginWritePath = true;
                         } else if (beginWritePath) {
@@ -102,23 +114,75 @@ public class PhDProjectScriptsParser {
         } else {
 
             String lineNumber = "";
-            String method = "";
-            String variable = "";
+            String methodName = "";
+            String variableName = "";
+            Smell sm;
 
             switch (smell) {
 
                 case "Switch Statement" -> {
                     String[] switchSmell = line.split("[()']+");
                     lineNumber = switchSmell[1].trim();
-                    method = switchSmell[3].trim();
-                    //Scrivere su DB//
+                    methodName = switchSmell[3].trim();
+
+                    try {
+
+                        //idClass
+                        int existsClass = DbHandler.classInProject(classes.get(0), projectUrl);
+                        c1 = new Class(classes.get(0), pathClasses.get(0));
+                        if(existsClass==0) {
+                            c1.setId(DbHandler.insertClass(c1));
+                        } else {
+                            String path = DbHandler.getClassPath(existsClass);
+                            if(path==null) {
+                                DbHandler.updateClassPath(pathClasses.get(0), existsClass);
+                                c1.setId(existsClass);
+                            } else {
+                                if(!DbHandler.getClassPath(existsClass).equals(classes.get(0)))
+                                    c1.setId(DbHandler.insertClass(c1));
+                                else
+                                    c1.setId(existsClass);
+                            }
+                        }
+
+                        //idMethod
+                        m = new Method();
+                        m.setName(methodName);
+                        int existsMethod = DbHandler.MethodInClass(m.getName(), (int) c1.getId());
+                        if (existsMethod == 0) {
+                            existsMethod = DbHandler.insertMethod(m, (int) c1.getId());
+                            m.setId(existsMethod);
+                        } else {
+                            m.setId(existsMethod);
+                        }
+
+                        //idPackage
+                        p = new Package(packages.get(0));
+                        int indexOfClass = (pathClasses.get(0)).indexOf(classes.get(0));
+                        String packagePath = (pathClasses.get(0)).substring(0, indexOfClass);
+                        p.setPath(packagePath);
+                        int existsPackage = DbHandler.packageExist(packages.get(0));
+                        if(existsPackage == 0) {
+                            existsPackage = DbHandler.insertPackage(p);
+                            p.setId(existsPackage);
+                        } else {
+                            p.setId(existsPackage);
+                        }
+
+
+                        //Writing on DB
+                        sm = new Smell("Switch Statement", 0.0);
+                        DbHandler.insertSmell(sm, idCommit, existsMethod, existsClass, existsPackage);
+
+                    } catch(SQLException e) {e.printStackTrace();}
+
                 }
                 case "Speculative Generality" -> {
                     String[] speculativeSmell = line.split("[()']+");
                     lineNumber = speculativeSmell[1].trim();
                     if (line.startsWith("Speculative Generality unused parameter")) {
-                        variable = speculativeSmell[3].trim();
-                        method = speculativeSmell[5].trim();
+                        variableName = speculativeSmell[3].trim();
+                        methodName = speculativeSmell[5].trim();
                     }
                     //Scrivere su DB//
                 }
@@ -138,7 +202,7 @@ public class PhDProjectScriptsParser {
                         String[] strings = line.split("[//(//)]+", 3);
                         String[] chainsSmell = strings[2].split("[\\[\\]]+");
                         lineNumber = strings[1].trim();
-                        method = chainsSmell[1].trim();
+                        methodName = chainsSmell[1].trim();
                         //Scrivere su DB//
                     }
                 }
@@ -146,12 +210,12 @@ public class PhDProjectScriptsParser {
                     String[] clumpsSmell;
                     if (line.startsWith("Parameters in method")) {
                         clumpsSmell = line.split("Parameters in method | and | was found duplicated");
-                        method = clumpsSmell[1].substring(clumpsSmell[1].substring(0, clumpsSmell[1].lastIndexOf(".")).lastIndexOf(".") + 1) + " & " + clumpsSmell[2].substring(clumpsSmell[2].substring(0, clumpsSmell[2].lastIndexOf(".")).lastIndexOf(".") + 1);
+                        methodName = clumpsSmell[1].substring(clumpsSmell[1].substring(0, clumpsSmell[1].lastIndexOf(".")).lastIndexOf(".") + 1) + " & " + clumpsSmell[2].substring(clumpsSmell[2].substring(0, clumpsSmell[2].lastIndexOf(".")).lastIndexOf(".") + 1);
 
                         //Scrivere su DB//
                     } else if (line.startsWith("Fields")) {
                         clumpsSmell = line.split("Fields  | was found duplicated");
-                        variable = clumpsSmell[1];
+                        variableName = clumpsSmell[1];
 
                         //Scrivere su DB//
                     }
@@ -163,13 +227,20 @@ public class PhDProjectScriptsParser {
                 System.out.print(s + " ");
             }
             System.out.print(" SMELL: " + smell);
-            System.out.print(" PRINTLN VARIABLE: " + variable);
-            System.out.print(" PRINTLN METHOD: " + method);
+            System.out.print(" PRINTLN VARIABLE: " + variableName);
+            System.out.print(" PRINTLN METHOD: " + methodName);
             System.out.print(" PRINTLN LINENUMBER: " + lineNumber + "\n");
         }
     }
 
-    public String getAnalyzableFileName() {
-        return analyzableFileName;
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Federico\\Desktop\\Programmi\\Intelli J\\sbac\\src\\tools\\toolsfolder\\PhDProjectScripts\\result.xml";
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
