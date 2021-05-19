f122215a48930a15e1321860ded42cc92ed9c98b
Davide <altieri.davide.98@gmail.com>
2021-05-19 16:44:56 +0200

    new modifiche con multicodesmel

---
+++ src/models/MultiCodeSmell.java
@@ -0,0 +1,40 @@
+package models;
+
+public class MultiCodeSmell {
+    private long id;
+    private String methods, variables, idCommit, idp, idcl, idm, idv = "";
+
+    public MultiCodeSmell(String methods, String variables, String idCommit, String idp, String idcl, String idm, String idv) {
+        this.methods = methods;
+        this.variables = variables;
+        this.idCommit = idCommit;
+        this.idp = idp;
+        this.idcl = idcl;
+        this.idm = idm;
+        this.idv = idv;
+    }
+
+    public void setId(long id) { this.id = id; }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getMethods() { return methods; }
+
+    public String getVariables() { return variables; }
+
+    public String getIdCommit() { return idCommit; }
+
+    public String getIdp() { return idp; }
+
+    public String getIdcl() { return idcl; }
+
+    public String getIdm() { return idm; }
+
+    public String getIdv() { return idv; }
+
+    @Override
+    public String toString() { return "DataClumps {" + "id=" + id + ", methods=" + methods + ",variables=" + variables + "}"; };
+
+}
