781554fa9c11255c9febb06782f6161b35ceaaab
ggalanteCode <89396637+ggalanteCode@users.noreply.github.com>
2022-02-08 13:55:34 +0100

    Create NoTokenException.java

---
+++ src/main/java/exceptions/NoTokenException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+/**
+ * This exception is thrown when no PAT is inserted in the Git token request window
+ * @author Giovanni Galante
+ *
+ */
+public class NoTokenException extends Exception {}
